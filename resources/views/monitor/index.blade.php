8b5631e1b58af6e10bbe45808a43709bcce29b54
Theerasak Meebunrot <Theerasak.Meebunrot@dentsu360.com>
2017-02-07 13:06:20 +0700

    fixed error script.

--- resources/views/monitor/index.blade.php
+++ resources/views/monitor/index.blade.php
@@ -43,33 +43,7 @@
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-xs-12 col-md-4">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="box box-default">
-			            <div class="box-header with-border">
-			              <h3 class="box-title">Top 10 Impressions (Today)</h3>
-			            </div>
-			            <div class="box-body">
-			              <canvas id="pieImpl" style="height: 277px; width: 555px;" height="554" width="1110"></canvas>
-			            </div>
-			        </div>
-		        </div>
-			</div>
-			<div class="row">
-				<div class="col-md-12">
-					<div class="box box-default">
-			            <div class="box-header with-border">
-			              <h3 class="box-title">Top 10 Spending (Today)</h3>
-			            </div>
-			            <div class="box-body">
-			              <canvas id="pieCost" style="height: 277px; width: 555px;" height="554" width="1110"></canvas>
-			            </div>
-			        </div>
-			    </div>
-			</div>
-		</div>
-		<div class="col-xs-12 col-md-8">
+		<div class="col-xs-12 col-md-12">
 			<div class="box">
 	            {{-- <div class="box-header">
 	              <h3 class="box-title">All Project</h3>
@@ -79,7 +53,7 @@
 	                <thead>
 		                <tr>
 		                  <th>Campaigns</th>
-		                  <th>Situations</th>
+		                  <th><center>Situations</center></th>
 		                  <th><center>Status</center></th>
 		                  <th><center>Period</center></th>
 		                  <th><center>Actions</center></th>
@@ -334,114 +308,6 @@
       "ordering": false,
       "info": false,
       "autoWidth": true
-    });
-    $(function() {
-    	var pieImplChartCanvas = $("#pieImpl").get(0).getContext("2d");
-	    var pieImplChart = new Chart(pieImplChartCanvas);
-	    var PieImplData = [
-	      {
-	        value: 700,
-	        color: "#f56954",
-	        highlight: "#f56954",
-	        label: "Chrome"
-	      },
-	      {
-	        value: 500,
-	        color: "#00a65a",
-	        highlight: "#00a65a",
-	        label: "IE"
-	      },
-	      {
-	        value: 400,
-	        color: "#f39c12",
-	        highlight: "#f39c12",
-	        label: "FireFox"
-	      },
-	      {
-	        value: 600,
-	        color: "#00c0ef",
-	        highlight: "#00c0ef",
-	        label: "Safari"
-	      },
-	      {
-	        value: 300,
-	        color: "#3c8dbc",
-	        highlight: "#3c8dbc",
-	        label: "Opera"
-	      },
-	      {
-	        value: 100,
-	        color: "#d2d6de",
-	        highlight: "#d2d6de",
-	        label: "Navigator"
-	      }
-	    ];
-	    var pieCostChartCanvas = $("#pieCost").get(0).getContext("2d");
-	    var pieCostChart = new Chart(pieCostChartCanvas);
-	    var PieCostData = [
-	      {
-	        value: 700,
-	        color: "#f56954",
-	        highlight: "#f56954",
-	        label: "Chrome"
-	      },
-	      {
-	        value: 500,
-	        color: "#00a65a",
-	        highlight: "#00a65a",
-	        label: "IE"
-	      },
-	      {
-	        value: 400,
-	        color: "#f39c12",
-	        highlight: "#f39c12",
-	        label: "FireFox"
-	      },
-	      {
-	        value: 600,
-	        color: "#00c0ef",
-	        highlight: "#00c0ef",
-	        label: "Safari"
-	      },
-	      {
-	        value: 300,
-	        color: "#3c8dbc",
-	        highlight: "#3c8dbc",
-	        label: "Opera"
-	      },
-	      {
-	        value: 100,
-	        color: "#d2d6de",
-	        highlight: "#d2d6de",
-	        label: "Navigator"
-	      }
-	    ];
-	    var pieOptions = {
-	      //Boolean - Whether we should show a stroke on each segment
-	      segmentShowStroke: true,
-	      //String - The colour of each segment stroke
-	      segmentStrokeColor: "#fff",
-	      //Number - The width of each segment stroke
-	      segmentStrokeWidth: 2,
-	      //Number - The percentage of the chart that we cut out of the middle
-	      percentageInnerCutout: 50, // This is 0 for Pie charts
-	      //Number - Amount of animation steps
-	      animationSteps: 100,
-	      //String - Animation easing effect
-	      animationEasing: "easeOutBounce",
-	      //Boolean - Whether we animate the rotation of the Doughnut
-	      animateRotate: true,
-	      //Boolean - Whether we animate scaling the Doughnut from the centre
-	      animateScale: false,
-	      //Boolean - whether to make the chart responsive to window resizing
-	      responsive: true,
-	      // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-	      maintainAspectRatio: true,
-	      //String - A legend template
-	      legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<segments.length; i++){%><li><span style=\"background-color:<%=segments[i].fillColor%>\"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>"
-	    };
-	    pieImplChart.Doughnut(PieImplData, pieOptions);
-	    pieCostChart.Doughnut(PieCostData, pieOptions);
     });
 </script>
 @endsection
--- resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/scripts.blade.php
@@ -2,13 +2,13 @@
 
 <!-- JQuery and bootstrap are required by Laravel 5.3 in resources/assets/js/bootstrap.js-->
 <!-- Laravel App -->
-{{-- <script src="{{ asset('/js/app.js') }}" type="text/javascript"></script> --}}
 <script src="/plugins/jQuery/jquery-2.2.3.min.js"></script>
-<script src="/plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="/plugins/datatables/dataTables.bootstrap.min.js"></script>
 <script src="/plugins/slimScroll/jquery.slimscroll.min.js"></script>
 <script src="/plugins/fastclick/fastclick.js"></script>
-<script src="/plugins/chartjs/Chart.min.js"></script>
+<script src="{{ asset('/js/app.js') }}" type="text/javascript"></script>
+<script src="/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="/plugins/datatables/dataTables.bootstrap.min.js"></script>
+
 <script>
     window.Laravel = {!! json_encode([
         'csrfToken' => csrf_token(),
